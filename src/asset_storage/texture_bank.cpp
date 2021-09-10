1b8720b1e9331b775b861cc8192bfea086784f40
Jakub Michalski <jakub.michalski2000@outlook.com>
2021-09-10 13:04:49 +0200

    Texture_bank::getTexture(): replaced duplicated find()

--- src/asset_storage/texture_bank.cpp
+++ src/asset_storage/texture_bank.cpp
@@ -1,22 +1,21 @@
 #include "texture_bank.h"
 
+#include <string_view>
+
 
 std::map<std::string, sf::Texture> Texture_bank::texture_map = std::map<std::string, sf::Texture>();
 
 const sf::Texture* Texture_bank::getTexture(const std::string& key)
 {
     auto it = texture_map.find(key);
-    if (texture_map.find(key) != texture_map.end())
+    if (it != texture_map.end())
     {
         return &it->second; 
     }
     else
     {
         sf::Texture tex;
-        if (!tex.loadFromFile(key))
-        {
-            //throw;
-        }
+        tex.loadFromFile(key);
         texture_map.insert({ key, tex });
 
         return &texture_map[key];
