ef6ffc109b14c94c7fef6d7d036c6682b39e2087
Jakub Michalski <jakub.michalski2000@outlook.com>
2021-10-01 12:56:06 +0200

    changed floor textures

--- src/asset_storage/tile_sprite_list.h
+++ src/asset_storage/tile_sprite_list.h
@@ -1,64 +1,17 @@
 { 
     ROCK | FLOOR, //tile id
     { 
-        {"assets/tiles/stone_floor.png", {0,0,64,64}}, //tile id variant
+        {"assets/tiles/stone_floor.png", {0,  0,64,64}, 0.25}, //tile id variant
+        {"assets/tiles/stone_floor.png", {64, 0,64,64}, 0.25},
+        {"assets/tiles/stone_floor.png", {128,0,64,64}, 0.25},
+        {"assets/tiles/stone_floor.png", {192,0,64,64}, 0.25},
     }
 },
 { 
     WOOD | FLOOR,
     { 
-        {"assets/tiles/wooden_floor.png", {0,  0,64,64}, 0.80},
-        {"assets/tiles/wooden_floor.png", {64, 0,64,64}, 0.10},
-        {"assets/tiles/wooden_floor.png", {128,0,64,64}, 0.05},
-        {"assets/tiles/wooden_floor.png", {192,0,64,64}, 0.05}
-    }
-},
-{
-    WOOD | FLOOR | L,
-    {
-        {"assets/tiles/wooden_floor_l.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | L | T,
-    {
-        {"assets/tiles/wooden_floor_l.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | L | B,
-    {
-        {"assets/tiles/wooden_floor_l.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | L | T | B,
-    {
-        {"assets/tiles/wooden_floor_l.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | R,
-    {
-        {"assets/tiles/wooden_floor_r.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | R | T,
-    {
-        {"assets/tiles/wooden_floor_r.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | R | B,
-    {
-        {"assets/tiles/wooden_floor_r.png", {0,0,64,64}}
-    }
-},
-{
-    WOOD | FLOOR | R | T | B,
-    {
-        {"assets/tiles/wooden_floor_r.png", {0,0,64,64}}
+        {"assets/tiles/wooden_floor.png", {0,  0,64,64}, 0.5},
+        {"assets/tiles/wooden_floor.png", {64, 0,64,64}, 0.5}
     }
 },
 {
@@ -166,54 +119,63 @@
 {
     FLOOR | CARPET,
     {
-        {"assets/tiles/carpet_overlay.png", {0,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {0,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {0,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | L | T,
     {
-        {"assets/tiles/carpet_overlay.png", {64,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {64,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {64,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | R | T,
     {
-        {"assets/tiles/carpet_overlay.png", {128,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {128,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {128,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | L | B,
     {
-        {"assets/tiles/carpet_overlay.png", {192,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {192,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {192,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | R | B,
     {
-        {"assets/tiles/carpet_overlay.png", {256,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {256,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {256,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | T,
     {
-        {"assets/tiles/carpet_overlay.png", {320,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {320,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {320,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | B,
     {
-        {"assets/tiles/carpet_overlay.png", {384,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {384,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {384,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | L,
     {
-        {"assets/tiles/carpet_overlay.png", {448,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {448,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {448,64,64,64}, 0.2}
     }
 },
 {
     FLOOR | CARPET | R,
     {
-        {"assets/tiles/carpet_overlay.png", {512,0,64,64}}
+        {"assets/tiles/carpet_overlay.png", {512,0, 64,64}, 0.8},
+        {"assets/tiles/carpet_overlay.png", {512,64,64,64}, 0.2}
     }
 },
