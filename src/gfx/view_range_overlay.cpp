67c694acfea93a1e23d7c36e019d2160c12db82e
Jakub Michalski <jakub.michalski2000@outlook.com>
2022-08-05 16:58:01 +0200

    Reduce shadow redraws

--- src/gfx/view_range_overlay.cpp
+++ src/gfx/view_range_overlay.cpp
@@ -19,8 +19,46 @@ void View_range_overlay::draw(sf::RenderTarget& rt, sf::RenderStates st) const
 {
     const sf::View current_view = rt.getView();
     rt.setView(sf::View{ sf::FloatRect{ { 0.f,0.f }, sf::Vector2f{ rt.getSize() } } });
-    rt.draw(sf::Sprite{ overlay_tex.getTexture() }, &blur_shader);
+    rt.draw(sf::Sprite{ overlay_tex.getTexture() });
     rt.setView(current_view);
+}
+
+void View_range_overlay::render(const Level& l, const pos_tvi_id_vec& visible, const pos_tvi_id_vec& revealed, const sf::RenderTarget& rt)
+{
+    if (overlay_tex.getSize() != rt.getSize())
+    {
+        overlay_tex.create(rt.getSize().x, rt.getSize().y);
+        work_tex.create(rt.getSize().x, rt.getSize().y);
+    }
+    work_tex.clear({ 0,0,0,255 });
+    work_tex.setView(rt.getView());
+
+    for (const auto& [pos, id] : revealed)
+    {
+        drawTileOverlay(pos, id);
+    }
+
+    sf::RectangleShape dark_overlay(sf::Vector2f{ work_tex.getSize() });
+    dark_overlay.setFillColor({ 0,0,0,224 });
+
+    work_tex.setView(sf::View{ sf::FloatRect{ { 0,0 }, sf::Vector2f{ work_tex.getSize() } } });
+    work_tex.draw(dark_overlay);
+    work_tex.setView(rt.getView());
+
+    for (const auto& [pos, id] : visible)
+    {
+        drawTileOverlay(pos, id);
+    }
+
+    work_tex.display();
+
+    blur_shader.setUniform("texture_size", sf::Vector2f{ work_tex.getSize() });
+    const float radius_mul = std::min(1.f, rt.getSize().x / rt.getView().getSize().x);
+    blur_shader.setUniform("radius", static_cast<int>(std::max(1.f, 8 * radius_mul)));
+
+    overlay_tex.clear({0,0,0,0});
+    overlay_tex.draw(sf::Sprite{ work_tex.getTexture() }, &blur_shader);
+    overlay_tex.display();
 }
 
 View_range_overlay::View_range_overlay()
@@ -76,80 +114,79 @@ View_range_overlay::View_range_overlay()
     blur_shader.setUniform("quality", 1);
 }
 
-void View_range_overlay::update(const Level& l, 
-                                const std::vector<std::pair<sf::Vector2i, Tile_visibility_info>>& visible_tiles,
-                                const Tile_reveal_mask& revealed_tiles, 
-                                const sf::RenderTarget& rt)
+void View_range_overlay::update(
+    const Level& l, 
+    const std::vector<std::pair<sf::Vector2i, Tile_visibility_info>>& visible_tiles,
+    const Tile_reveal_mask& revealed_tiles, 
+    const sf::RenderTarget& rt)
 {
+    const auto id = [&](const sf::Vector2i& position, const Tile_visibility_info tvi, const bool visible)
     {
-        if(overlay_tex.getSize() != rt.getSize()) overlay_tex.create(rt.getSize().x, rt.getSize().y);
-        overlay_tex.clear({ 0,0,0,255 });
-        overlay_tex.setView(rt.getView());
+        const auto& [tl, tr, bl, br] = tvi;
+        if (l.getStructure().at(position).type == TILE_TYPE::WALL
+            || l.getEntities().forEachUntil(position, [&](auto& entity) { return visible ? l.getRegistry().all_of<Door, Opaque>(entity.second) : l.getRegistry().all_of<Door>(entity.second); }))
+        {
+            return tl * TL | tr * TR | bl * BL | br * BR;
+        }
+        return TL | TR | BL | BR;
+    };
 
-        blur_shader.setUniform("texture_size", sf::Vector2f{ overlay_tex.getSize() });
-        const float radius_mul = std::min(1.f, rt.getSize().x / rt.getView().getSize().x);
-        blur_shader.setUniform("radius", static_cast<int>(std::max(1.f, 8*radius_mul)));
+    auto visible = [&]
+    {
+        decltype(last_visible) visible;
+        for (const auto& [pos, tvi] : visible_tiles)
+        {
+            if (tvi.isVisible() && l.getStructure().isPositionValid(pos)) visible.emplace_back(pos, id(pos, tvi, true));
+        }
+        return visible;
+    }();
 
-        const auto [tl_tile, br_tile] = [&]()
+    auto revealed = [&]
+    {
+        decltype(last_revealed) revealed;
+
+        const auto [tl_tile, br_tile] = [&]
         {
             const auto bounds = visibleAreaBoundsTiles(rt.getView());
             const sf::Vector2i tl_tile = { std::max(0, bounds.first.x),
                                            std::max(0, bounds.first.y) };
             const sf::Vector2i br_tile = { std::min(l.getStructure().getSize().x - 1, bounds.second.x),
-                                           std::min(l.getStructure().getSize().y-1, bounds.second.y) };
+                                           std::min(l.getStructure().getSize().y - 1, bounds.second.y) };
             return std::pair{ tl_tile, br_tile };
         }();
 
-        for (int x = tl_tile.x; x <= br_tile.x; x++)
+        for (int x = tl_tile.x; x < br_tile.x; x++)
         {
-            for (int y = tl_tile.y; y <= br_tile.y; y++)
+            for (int y = tl_tile.y; y < br_tile.y; y++)
             {
-                if (revealed_tiles.at({ x,y }).isVisible())
-                {
-                    drawTileOverlay(l, { x,y }, revealed_tiles.at({ x,y }), false);
-                }
+                const auto pos = sf::Vector2i{ x,y };
+                const auto tvi = revealed_tiles.at(pos);
+                if (tvi.isVisible()) revealed.emplace_back(pos, id(pos, tvi, false));
             }
         }
+        return revealed;
+    }();  
 
-        sf::RectangleShape dark_overlay(sf::Vector2f{ overlay_tex.getSize() });
-        dark_overlay.setFillColor({ 0,0,0,224 });
+    if (   last_view.getCenter() != rt.getView().getCenter()
+        || last_view.getSize() != rt.getView().getSize()
+        || last_view.getTransform() != rt.getView().getTransform()
+        || overlay_tex.getSize() != rt.getSize()
+        || last_visible != visible
+        || last_revealed != revealed)
+    {
+        render(l, visible, revealed, rt);
 
-        overlay_tex.setView(sf::View{ sf::FloatRect{ { 0,0 }, sf::Vector2f{ overlay_tex.getSize() } } });
-        overlay_tex.draw(dark_overlay);
-        overlay_tex.setView(rt.getView());
-
-        for (const auto& tile : visible_tiles)
-        {
-            const auto& [position, tvi] = tile;
-            if (l.getStructure().isPositionValid(position))
-            {
-                drawTileOverlay(l, position, tvi, true);
-            }
-        }
-
-        overlay_tex.display();
+        last_view = rt.getView();
+        last_visible = std::move(visible);
+        last_revealed = std::move(revealed);
     }
 }
 
-void View_range_overlay::drawTileOverlay(const Level& l, 
-                                         const sf::Vector2i& position,
-                                         const Tile_visibility_info tvi,
-                                         const bool visible)
+void View_range_overlay::drawTileOverlay(const sf::Vector2i& position, const TILE_SPRITE_ID::tile_sprite_id_t id)
 {
-    const tile_sprite_id_t id = [&]
-    {
-        const auto& [tl, tr, bl, br] = tvi;
-        if (l.getStructure().at(position).type == TILE_TYPE::WALL 
-            || l.getEntities().forEachUntil(position, [&](auto& entity) { return visible ? l.getRegistry().all_of<Door, Opaque>(entity.second) : l.getRegistry().all_of<Door>(entity.second); }))
-        {
-            return tl * TL | tr * TR | bl * BL | br * BR;
-        }
-        return TL | TR | BL | BR;       
-    }();
-
     if (auto it = sprites.find(id); it != sprites.end())
     {
         it->second.setPosition(sf::Vector2f{ vecMul(position, Tile_sprite_storage::tile_size) });
-        overlay_tex.draw(it->second, sf::BlendMultiply);
+        work_tex.draw(it->second, sf::BlendMultiply);
     }
 }
--- src/gfx/view_range_overlay.h
+++ src/gfx/view_range_overlay.h
@@ -13,21 +13,35 @@
 class View_range_overlay : public sf::Drawable
 {
     sf::RenderTexture overlay_tex;
+    sf::RenderTexture work_tex;
+
     sf::Shader blur_shader;
 
     std::map<TILE_SPRITE_ID::tile_sprite_id_t, sf::Sprite> sprites;
 
+    using pos_tvi_id_vec = std::vector<std::pair<sf::Vector2i, TILE_SPRITE_ID::tile_sprite_id_t>>;
+    pos_tvi_id_vec last_visible;
+    pos_tvi_id_vec last_revealed;
+    sf::View last_view;
+
     void draw(sf::RenderTarget& rt, sf::RenderStates st) const override;
 
-    void drawTileOverlay(const Level& l,
-                         const sf::Vector2i& position,
-                         const Tile_visibility_info tvi,
-                         const bool visible);
+    void render(
+        const Level& l,
+        const pos_tvi_id_vec& visible,
+        const pos_tvi_id_vec& revealed,
+        const sf::RenderTarget& rt
+    );
+
+    void drawTileOverlay( const sf::Vector2i& position, const TILE_SPRITE_ID::tile_sprite_id_t id);
 
 public:
     View_range_overlay();
-    void update(const Level& l,
-                const std::vector<std::pair<sf::Vector2i, Tile_visibility_info>>& visible_tiles,
-                const Tile_reveal_mask& revealed_tiles,
-                const sf::RenderTarget& rt);
+
+    void update(
+        const Level& l,
+        const std::vector<std::pair<sf::Vector2i, Tile_visibility_info>>& visible_tiles,
+        const Tile_reveal_mask& revealed_tiles,
+        const sf::RenderTarget& rt
+    );
 };
